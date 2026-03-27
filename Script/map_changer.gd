d41e8d8fb51a7e534838a80b1f855e33a7fa294b
Ewen R <106588588+Miit0s@users.noreply.github.com>
2026-03-27 17:06:18 +0100

    V1.0 TP VR

--- Script/map_changer.gd
+++ Script/map_changer.gd
@@ -19,16 +19,25 @@ func _ready() -> void:
 func switch_map():
 	var map_instance: Room = maps.pop_front().instantiate()
 	
-	if current_map != null: remove_child(current_map)
+	if current_map != null: 
+		current_map.button_pressed.disconnect(_on_button_clicked)
+		current_map.collider_trigger.disconnect(_on_collider_trigger)
+		call_deferred("remove_child", current_map)
 	add_child(map_instance)
 	current_map = map_instance
 	
 	_number_of_click_needed = randi_range(min_click_before_change, max_click_before_change)
 	
 	current_map.button_pressed.connect(_on_button_clicked)
+	current_map.collider_trigger.connect(_on_collider_trigger)
 
 func validate_room_button():
 	current_map.validate_button()
+
+func reset_map():
+	number_of_click = 0
+	player.update_click_count(str(number_of_click))
+	_room_need_to_be_reset = false
 
 func _on_button_clicked():
 	number_of_click += 1
@@ -40,4 +49,5 @@ func _on_button_clicked():
 	if number_of_click >= value_to_reach: validate_room_button()
 
 func _on_collider_trigger():
-	pass
+	reset_map()
+	switch_map()
--- Script/room.gd
+++ Script/room.gd
@@ -2,12 +2,21 @@ extends Node3D
 class_name Room
 
 signal button_pressed
-signal collider_tricker
+signal collider_trigger
 
 @export var button: BasicButton
+@export var door: Node3D
 
 func _on_basic_button_button_pressed() -> void:
 	button_pressed.emit()
 
 func validate_button():
 	button.validate_button()
+
+
+func _on_area_3d_body_entered(_body: Node3D) -> void:
+	collider_trigger.emit()
+
+
+func _on_open_door_area_body_entered(_body: Node3D) -> void:
+	door.hide()
